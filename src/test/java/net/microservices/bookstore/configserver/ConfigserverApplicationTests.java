49750c0b8407d6910234f31ac040309e24a52ebd
Vu Cao <vu.cao@uxcoxygen.com>
2020-10-06 23:24:12 +1100

    Initial commit

---
+++ src/test/java/net/microservices/bookstore/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,16 @@
+package net.microservices.bookstore.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class ConfigserverApplicationTests
+{
+
+	@Test
+	void contextLoads()
+	{
+	}
+
+}
